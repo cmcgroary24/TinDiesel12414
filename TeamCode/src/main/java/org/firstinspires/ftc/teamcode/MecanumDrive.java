af514ffa795b07ae1564cf09763f9434fe378cee
bbuatte24 <bbuatte24@klschools.org>
2022-10-06 10:54:24 -0400

    pushhhh

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 //yo this works
 //test
+//i can push
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
