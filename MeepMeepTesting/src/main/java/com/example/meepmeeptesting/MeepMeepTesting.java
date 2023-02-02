b49d98f47534a1e0fea6671b08c099b391bee047
cmcgroary24 <cmcgroary24@klschools.org>
2023-02-02 14:57:03 -0500

    meepmeep

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,27 +1,32 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
+
+    public final static double SERVO_HOME = 0.5;
+    public final static double SERVO_MIN_RANGE = 0.5;
+    public final static double SERVO_MAX_RANGE = 0.63;
+
+    public static final double d180 = Math.toRadians(180);
+    public static final double d0 = Math.toRadians(0);
+    public static final double d90 = Math.toRadians(90);
+    public static final double d270 = Math.toRadians(270);
+
     public static void main(String[] args) {
+
         MeepMeep meepMeep = new MeepMeep(800);
+
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
+                        drive.trajectorySequenceBuilder(new Pose2d(36, 63, Math.toRadians(180)))
                                 .build()
                 );
 
